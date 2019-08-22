d39c07443db23b53ca692a4f8b5095a252ddd0ab
fakhriabdullah <fakhri.abdullah9496@gmail.com>
2019-08-22 07:17:18 +0700

    home

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,157 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+            primaryColor: Colors.amber, accentColor: Colors.amberAccent),
+        home: HomeView());
+  }
+}
+
+class HomeView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: NestedScrollView(
+          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+            return <Widget>[
+              SliverAppBar(
+                expandedHeight: 80,
+                pinned: true,
+                floating: false,
+                flexibleSpace: FlexibleSpaceBar(
+                  centerTitle: true,
+                  title: Image.asset(
+                    "assets/logo-zenius.png",
+                    width: 90,
+                  ),
+                  background: Image.network(
+                    "https://images.pexels.com/photos/396547/pexels-photo-396547.jpeg?auto=compress&cs=tinysrgb&h=350",
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ];
+          },
+          body: BodyHome()),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class BodyHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Container(
+            color: Colors.amber,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.fromLTRB(12, 8, 12, 0),
+                  child: Text("Hi, Fakhri Abdullah"),
+                ),
+                Identity(),
+                Search(),
+              ],
+            )
+        )
+      ],
+    );
+  }
+}
+
+class Identity extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        color: Colors.amberAccent,
+        elevation: 0,
+        margin: EdgeInsets.fromLTRB(12, 4, 12, 4),
+        child: Padding(
+          padding: EdgeInsets.all(8),
+          child: Row(
+            children: <Widget>[
+              Flexible(child: Image.asset("assets/logo-zenius.png"), flex: 1),
+              Flexible(
+                  child: Padding(
+                      padding: EdgeInsets.fromLTRB(8, 0, 0, 0),
+                      child: Column(
+                        children: <Widget>[
+                          Align(
+                              alignment: Alignment.centerLeft,
+                              child: Text("fakhri.abdullah9496@gmail.com",
+                                  style: TextStyle(
+                                      fontSize: 12,
+                                      fontStyle: FontStyle.italic))),
+                          Align(
+                              alignment: Alignment.centerLeft,
+                              child: Text("Fakhri Abdullah",
+                                  style: TextStyle(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.bold))),
+                          Container(
+                            margin: EdgeInsets.fromLTRB(0, 8, 0, 0),
+                            child: Row(
+                              children: <Widget>[
+                                Flexible(
+                                    child: Text("KTSP",
+                                        style: TextStyle(fontSize: 11)),
+                                    flex: 1,
+                                    fit: FlexFit.tight),
+                                Flexible(
+                                    child: Text("Reguler Member",
+                                        style: TextStyle(fontSize: 11)),
+                                    flex: 1,
+                                    fit: FlexFit.tight)
+                              ],
+                            ),
+                          ),
+                          Row(
+                            children: <Widget>[
+                              Flexible(
+                                  child: Text("Alumni",
+                                      style: TextStyle(fontSize: 11)),
+                                  flex: 1,
+                                  fit: FlexFit.tight),
+                              Flexible(
+                                  child: Text("Kelas 12 Umum",
+                                      style: TextStyle(fontSize: 11)),
+                                  flex: 1,
+                                  fit: FlexFit.tight)
+                            ],
+                          )
+                        ],
+                      )),
+                  flex: 2),
+            ],
+          ),
+        ));
+  }
+}
+
+class Search extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.fromLTRB(12, 8, 12, 8),
+      color: Colors.amberAccent,
+      child: TextField(
+          decoration: new InputDecoration(
+            contentPadding: EdgeInsets.all(12),
+            border: InputBorder.none,
+            hintText: 'Search',
+            prefixIcon: Icon(Icons.search)
+          )
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
